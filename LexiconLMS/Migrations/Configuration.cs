5c510ab7ad7f3f06aa2ea6aad87527114b5f4a19
Ayesha Azam <ayesh76@msn.com>
2017-07-20 11:45:23 +0200

    Added Seed data

--- LexiconLMS/Migrations/Configuration.cs
+++ LexiconLMS/Migrations/Configuration.cs
@@ -1,7 +1,9 @@
 namespace LexiconLMS.Migrations
 {
+    using LexiconLMS.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
@@ -9,23 +11,69 @@ namespace LexiconLMS.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(LexiconLMS.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+
+            var roleNames = new[] { "Teacher", "Student" };
+            foreach (var roleName in roleNames)
+            {
+                if(!context.Roles.Any(r => r.Name ==roleName))
+                {
+                    //creates Roll
+                    var role = new IdentityRole { Name = roleName };
+
+                    var result = roleManager.Create(role);
+                    if(!result.Succeeded)
+                    {
+                        throw new Exception(string.Join("\n", result.Errors));
+                    }
+                }
+            }
+
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var emails = new[] { "user@lexicon.se", "teacher@lexicon.se", "student@lexicon.se" };
+            foreach (var email in emails)
+            {
+              if(!context.Users.Any(u => u.UserName == email))
+                {
+
+                    //creating user
+                    var user = new ApplicationUser
+                    {
+                        UserName = email,
+                        Email = email,
+                        UserStartDate = DateTime.Now
+                    };
+
+                    var result = userManager.Create(user, "foobar");
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(string.Join("\n", result.Errors));
+                    }
+
+                }
+            }
+
+            var teacherUser = userManager.FindByName("teacher@lexicon.se");
+            userManager.AddToRole(teacherUser.Id, "Teacher");
+            //teacherUser.UserFirstName.Replace(Null, "Teacher");
+
+
+            var studentUser = userManager.FindByName("student@lexicon.se");
+            userManager.AddToRole(teacherUser.Id, "Student");
+            //teacherUser.UserFirstName.Replace(null, "Student");
+
         }
     }
 }
