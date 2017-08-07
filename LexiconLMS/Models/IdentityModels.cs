c7e7ac484c056194937521a1111639499a550c44
Ayesha Azam <ayesh76@msn.com>
2017-08-07 11:32:33 +0200

    Small issue was fixed

--- LexiconLMS/Models/IdentityModels.cs
+++ LexiconLMS/Models/IdentityModels.cs
@@ -1,10 +1,10 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace LexiconLMS.Models
 {
