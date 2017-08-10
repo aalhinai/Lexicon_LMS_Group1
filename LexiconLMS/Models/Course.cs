c3209daa38b3006ca5e6e5904dcddb0f7e122904
envh <e.niklashagman@gmail.com>
2017-08-10 09:30:07 +0200

    Added display names.

--- LexiconLMS/Models/Course.cs
+++ LexiconLMS/Models/Course.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LexiconLMS.Models
 {
     public class Course
     {
         public int CourseId { get; set; }
+
+        [Display(Name = "Name")]
         public string CourseName { get; set; }
+
+        [Display(Name = "Start Date")]
         public DateTime CourseStartDate { get; set; }
+
+        [Display(Name = "End Date")]
         public DateTime CourseEndDate { get; set; }
+
+        [Display(Name = "Description")]
         public string CourseDescription { get; set; }
 
         public virtual ICollection<ApplicationUser> Students { get; set; }
