0d0741e309efce6ae41fba9f477bf42d851ae7bf
AL- HINAI <theblueghost_99@hotmail.com>
2017-08-12 17:59:24 +0200

    copy and paste same configration file from orgin master github to local master branch

--- LexiconLMS/Migrations/201708121552086_copy/paste same file from github after unexpected deletion by mistak.cs
+++ LexiconLMS/Migrations/201708121552086_copy/paste same file from github after unexpected deletion by mistak.cs
@@ -3,7 +3,7 @@ namespace LexiconLMS.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class AddedInit : DbMigration
+    public partial class copypastesamefilefromgithubafterunexpecteddeletionbymistak : DbMigration
     {
         public override void Up()
         {
